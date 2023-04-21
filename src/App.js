3642e3a2f36319daf2c92df5ae16b6aa5c493af1
Sallein <saleinkyl@gmail.com>
2023-04-21 19:14:12 +0300

    solution 5

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react"
-import "./App.css"
+import React from "react";
+import "./App.css";
 
 // function App() {
 //   const arr = [1, 2, 3, 4, 5]
-	
+
 // 	return <div>
 // 		<ul>
 //       {arr.map(i => <li>{i}</li>)}
@@ -39,15 +39,50 @@ import "./App.css"
 
 // export default App
 
-const App = () => {
-  const showMess = () => {
-    alert('Yo')
-  }
-  return (
-  <>
-  <button onClick={showMess} >Click</button>
-  </>
-)
-}
+// const App = () => {
+//   const showMess = () => {
+//     alert('Yo')
+//   }
+//   return (
+//   <>
+//   <button onClick={showMess} >Click</button>
+//   </>
+// )
+// }
 
-export default App
+// export default App
+
+const App = () => {
+  const users = [
+    { id: 1, name: "user1", surn: "surn1", age: 30 },
+    { id: 2, name: "user2", surn: "surn2", age: 31 },
+    { id: 3, name: "user3", surn: "surn3", age: 32 },
+  ];
+
+  const data = users.map((item) => {
+    return (
+      <tr key={item.id}>
+        <td>{item.name}</td>
+        <td>{item.surn}</td>
+        <td>{item.age}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <td>Имя</td>
+            <td>Фамилия</td>
+            <td>Возраст</td>
+          </tr>
+        </thead>
+        <tbody>{data}</tbody>
+      </table>
+    </>
+  );
+};
+
+export default App;
